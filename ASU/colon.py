15854fa8e2166a210e9152bad04f8110331ca63f
沈子骁 <shenzixiao@chenzixiaodeMacBook-Pro.local>
2019-09-16 16:42:27 +0100

    changes

--- ASU/colon.py
+++ ASU/colon.py
@@ -1,8 +1,8 @@
 import scipy.io
 
 mat = scipy.io.loadmat('/Users/shenzixiao/Dropbox/DATA/ASU/BiologicalData/colon.mat')
-X = mat['X']  # data
-x = X.astype(float)
-y = mat['Y']
-y = y[:, 0]
-n_samples, n_features = X.shape  # number of samples and number of features
+X = mat['X']
+X = X.astype(float)
+Y = mat['Y']
+Y = Y[:, 0]
+n_samples, n_features = X.shape
