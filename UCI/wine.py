4d62baca20fdae32f9e409799479ad5219addb1f
Zixiao Shen <ZealShen>
2019-10-04 22:34:24 +0100

    add wine datasets

--- UCI/wine.py
+++ UCI/wine.py
@@ -5,3 +5,7 @@ import urllib.request
 wine_url = "http://archive.ics.uci.edu//ml//machine-learning-databases//wine//wine.data"
 raw_data = urllib.request.urlopen(wine_url)
 raw_wine = np.loadtxt(raw_data, delimiter=",")
+
+X = raw_wine[:, 1:14]
+Y = raw_wine[:, 0].astype('int')
+n_samples, n_feature = X.shape  # number of samples and number of features
