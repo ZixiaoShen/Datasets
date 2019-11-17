bf2440896f5d7c7f2277bfb7f5a68b2995bf7883
Zixiao Shen <ZealShen>
2019-11-17 20:08:15 +0000

    changes

--- UCI/F8_PIMA/pima.py
+++ UCI/F8_PIMA/pima.py
@@ -5,7 +5,7 @@ import requests
 
 
 def pima():
-    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/PIMA/pima-indians-diabetes.data"
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/F8_PIMA/pima-indians-diabetes.data"
     s = requests.get(file_url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
     df.columns = ['No_pregnant', 'Plasma_glucose', 'Blood_pres',
