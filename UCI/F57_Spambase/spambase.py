608560ccd3fa1b27620c45e4227d10b6e2564aad
Zixiao Shen <ZealShen>
2019-11-18 11:58:57 +0000

    changes

--- UCI/F57_Spambase/spambase.py
+++ UCI/F57_Spambase/spambase.py
@@ -3,20 +3,20 @@ import io
 import requests
 
 
-def spectfheart():
+def spambase():
     file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/F57_Spambase/Spambase.csv"
     s = requests.get(file_url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
 
     data = df.values
-    x = data[:, 1:]
-    x = x.astype('int')
-    y = data[:, 0].astype('int')
+    x = data[:, 0:57]
+    x = x.astype('float')
+    y = data[:, 57].astype('int')
     return x, y
 
 
 if __name__ == '__main__':
-    X, Y = spectfheart()
+    X, Y = spambase()
     n_samples, n_features = X.shape
     print(n_samples)
     print(n_features)
