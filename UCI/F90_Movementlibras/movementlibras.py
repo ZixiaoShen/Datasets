830644253a3823e910920a61ccc723329a9b748c
Zixiao Shen <ZealShen>
2019-11-18 12:30:23 +0000

    add movementlibrasg

---
+++ UCI/F90_Movementlibras/movementlibras.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import io
+import requests
+
+
+def movementlibras():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/F90_Movementlibras/Movementlibras.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+
+    data = df.values
+    x = data[:, 0:57]
+    x = x.astype('float')
+    y = data[:, 57].astype('int')
+    return x, y
+
+
+if __name__ == '__main__':
+    X, Y = spambase()
+    n_samples, n_features = X.shape
+    print(n_samples)
+    print(n_features)
