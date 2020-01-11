346c47e7db99289bf261fe61259a0f17553a2b0a
ZealShen <zealshen@outlook.com>
2020-01-11 19:34:52 +0000

    changes

--- UCI/F21_Absenteeism/Absenteeism.py
+++ UCI/F21_Absenteeism/Absenteeism.py
@@ -3,20 +3,21 @@ import io
 import requests
 
 
-def appendicitis():
-    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/F7_Appendicitis/Appendicitis.csv"
+def absenteeism():
+    file_url = \
+        "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/F21_Absenteeism/Absenteeism_at_work.csv"
     s = requests.get(file_url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
-
+    df = df.drop([0])
     data = df.values
-    x = data[:, 0:7]
+    x = data[:, 0:20]
     x = x.astype('float')
-    y = data[:, 7].astype('int')
+    y = data[:, 20].astype('int')
     return x, y
 
 
 if __name__ == '__main__':
-    X, Y = appendicitis()
-    n_samples, n_features = X.shape
-    print(n_samples)
-    print(n_features)
+    X, Y = absenteeism()
+    # n_samples, n_features = X.shape
+    # print(n_samples)
+    # print(n_features)
