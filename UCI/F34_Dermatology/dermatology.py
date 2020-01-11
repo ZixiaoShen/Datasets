7e1c2115a0e0686489249c0d6fcaf663e5d82a7a
ZealShen <zealshen@outlook.com>
2020-01-11 17:40:11 +0000

    changes

---
+++ UCI/F34_Dermatology/dermatology.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import io
+import requests
+
+
+def dermatology():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/Dermatology/dermatology.data"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+
+    data = df.replace(to_replace="?", value=np.nan)
+    data = data.dropna(how='any')
+    dermatology = data.values
+    # dermatology = dermatology.astype('int')
+
+    x = dermatology[:, 0:34]
+    x = x.astype(int)
+    y = dermatology[:, 34].astype(int)
+    return x, y
+
+
+if __name__ == '__main__':
+    X, Y = dermatology()
+    n_samples, n_features = X.shape
+    print(n_samples)
+    print(n_features)
