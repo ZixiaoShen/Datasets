0f070d7ceef8de897ef6d23cede4fc734c1e30bd
ZealShen <zealshen@outlook.com>
2020-01-11 19:38:10 +0000

    changes

--- UCI/F21_Absenteeism/Absenteeism.py
+++ UCI/F21_Absenteeism/Absenteeism.py
@@ -11,13 +11,13 @@ def absenteeism():
     df = df.drop([0])
     data = df.values
     x = data[:, 0:20]
-    x = x.astype('float')
-    y = data[:, 20].astype('int')
+    x = x.astype(int)
+    y = data[:, 20].astype(int)
     return x, y
 
 
 if __name__ == '__main__':
     X, Y = absenteeism()
-    # n_samples, n_features = X.shape
-    # print(n_samples)
-    # print(n_features)
+    n_samples, n_features = X.shape
+    print(n_samples)
+    print(n_features)
