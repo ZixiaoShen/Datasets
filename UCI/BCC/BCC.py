57e47a025f73d75dddff69a5693a4f684d0316c5
ZixiaoShen <zealshen@outlook.com>
2020-02-05 20:19:32 +0000

    bcc

--- UCI/BCC/BCC.py
+++ UCI/BCC/BCC.py
@@ -3,24 +3,22 @@ import io
 import requests
 
 
-# def phoneme():
-file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-            "BCC/dataR2.csv"
-s = requests.get(file_url).content
-df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+def breast_cancer_coimbra():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+                "BCC/dataR2.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-    # data = df.values
-    # data = data[1:, :]
-    # x = data[:, 0:5]
-    # x = x.astype('float')
-    # y = data[:, 5].astype('int')
-    # return x, y
+    data = df.values
+    x = data[:, 0:9]
+    y = data[:, 9].astype('int')
+    return x, y
 
 
-# if __name__ == '__main__':
-#     X, Y = phoneme()
-#     n_samples, n_features = X.shape
-#     n_class = len(set(Y))
-#     print("Number of Samples:", n_samples)
-#     print("Number of Features:", n_features)
-#     print("Number of Classes:", n_class)
+if __name__ == '__main__':
+    X, Y = breast_cancer_coimbra()
+    n_samples, n_features = X.shape
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
