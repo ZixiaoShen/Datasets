6e69c4568201ef0e9ed7f197356ef58b671d8f99
ZixiaoShen <zealshen@outlook.com>
2020-02-05 20:22:18 +0000

    bcc

--- UCI/C2_F9_S116_BCC/BCC.py
+++ UCI/C2_F9_S116_BCC/BCC.py
@@ -5,7 +5,7 @@ import requests
 
 def breast_cancer_coimbra():
     file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-                "BCC/dataR2.csv"
+                "C2_F9_S116_BCC/dataR2.csv"
     s = requests.get(file_url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
