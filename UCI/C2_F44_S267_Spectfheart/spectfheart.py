66f367d7600e6e0c2ea597396a8aa415a458a60c
ZixiaoShen <zealshen@outlook.com>
2020-02-05 18:54:13 +0000

    changes

--- UCI/C2_F44_S267_Spectfheart/spectfheart.py
+++ UCI/C2_F44_S267_Spectfheart/spectfheart.py
@@ -18,5 +18,7 @@ def spectfheart():
 if __name__ == '__main__':
     X, Y = spectfheart()
     n_samples, n_features = X.shape
-    print(n_samples)
-    print(n_features)
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
--- UCI/C2_F60_S208_Sonar/sonar.py
+++ UCI/C2_F60_S208_Sonar/sonar.py
@@ -21,5 +21,7 @@ def sonar():
 if __name__ == '__main__':
     X, Y = sonar()
     n_samples, n_features = X.shape
-    print(n_samples)
-    print(n_features)
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
