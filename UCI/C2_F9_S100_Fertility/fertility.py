809c1858d648ba369565563112fa2e49b9d4a67a
ZealShen <zealshen@outlook.com>
2020-02-06 13:56:18 +0000

    hcv

--- UCI/C2_F9_S100_Fertility/fertility.py
+++ UCI/C2_F9_S100_Fertility/fertility.py
@@ -3,23 +3,23 @@ import io
 import requests
 
 
-# def appendicitis():
-file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-            "C2_F9_S100_Fertility/Fertility.csv"
-s = requests.get(file_url).content
-df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+def fertility():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+                "C2_F9_S100_Fertility/Fertility.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+    x_df = df.iloc[:, 0:9]
+    y_df = df.iloc[:, 9]
+    x = x_df.values
+    class_mapping = {'N': 0, 'O': 1}
+    y = y_df.map(class_mapping).values
+    return x, y
 
-    # data = df.values
-    # x = data[:, 0:7]
-    # x = x.astype('float')
-    # y = data[:, 7].astype('int')
-    # # return x, y
 
-#
-# if __name__ == '__main__':
-#     X, Y = appendicitis()
-#     n_samples, n_features = X.shape
-#     n_class = len(set(Y))
-#     print("Number of Samples:", n_samples)
-#     print("Number of Features:", n_features)
-#     print("Number of Classes:", n_class)
+if __name__ == '__main__':
+    X, Y = fertility()
+    n_samples, n_features = X.shape
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
