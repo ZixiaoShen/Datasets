9b1e1ad173327ff3abf9e93dedc2c7194f49fa04
ZealShen <zealshen@outlook.com>
2020-02-06 18:59:38 +0000

    leaf

--- UCI/Leaf/leaf.py
+++ UCI/Leaf/leaf.py
@@ -1,20 +1,18 @@
 import pandas as pd
-import numpy as np
 import io
 import requests
 
 
 def leaf():
     file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-               "Leaf/leaf.csv"
+                "Leaf/leaf.csv"
     s = requests.get(file_url).content
     df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
-    df = df.drop(columns=0)
-    x_df = df.drop(columns=10)
-    y_df = df[10]
+    x_df = df.drop(columns=0)
+    y_df = df.iloc[:, 0]
 
-    x = np.array(x_df)
-    y = np.array(y_df)
+    x = x_df.values
+    y = y_df.values
     return x, y
 
 
