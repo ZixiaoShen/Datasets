4ba8184647a3b2d47773247a3fef2e2529013a33
ZealShen <zealshen@outlook.com>
2020-02-06 15:53:20 +0000

    ilpd

--- UCI/ILPD/ILPD.py
+++ UCI/ILPD/ILPD.py
@@ -4,30 +4,27 @@ import io
 import requests
 
 
-# def ilpd():
-file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-            "ILPD/ILPD.csv"
-s = requests.get(file_url).content
-df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
-    # df_str = df[0]
-    # df1 = df.drop(columns=0)
-    # class_mapping = {'M': 0, 'F': 1, 'I': 2}
-    # df0 = df_str.map(class_mapping)
-    # df0 = df0.to_frame()
-    #
-    # data0 = df0.values
-    # data1 = df1.values
-    # data = np.hstack((data0, data1))
-    # x = data[:, 0:8]
-    # x = x.astype('float')
-    # y = data[:, 8].astype('int')
-    # return x, y
+def ilpd():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+                "ILPD/ILPD.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+
+    y = df.iloc[:, -1].values
+    x_df = df.iloc[:, 0:-1]
+
+    df_str = x_df.iloc[:, 1]
+    class_mapping = {'Male': 0, 'Female': 1}
+    df1 = df_str.map(class_mapping)
+    x_df.iloc[:, 1] = df1
+    x = x_df.values
+    return x, y
 
 
-# if __name__ == '__main__':
-#     X, Y = abalone()
-#     n_samples, n_features = X.shape
-#     n_class = len(set(Y))
-#     print("Number of Samples:", n_samples)
-#     print("Number of Features:", n_features)
-#     print("Number of Classes:", n_class)
+if __name__ == '__main__':
+    X, Y = ilpd()
+    n_samples, n_features = X.shape
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
