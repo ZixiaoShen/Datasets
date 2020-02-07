f17ab2dff1e91e6b831612438f078ee6283c91d2
ZealShen <zealshen@outlook.com>
2020-02-07 19:47:06 +0000

    mam

---
+++ UCI/Mammographic/mammographic.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import io
+import requests
+
+
+# def appendicitis():
+file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+            "C2_F7_S106_Appendicitis/Appendicitis.csv"
+s = requests.get(file_url).content
+df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
+
+    # data = df.values
+    # x = data[:, 0:7]
+    # x = x.astype('float')
+    # y = data[:, 7].astype('int')
+    # return x, y
+
+
+# if __name__ == '__main__':
+#     X, Y = appendicitis()
+#     n_samples, n_features = X.shape
+#     n_class = len(set(Y))
+#     print("Number of Samples:", n_samples)
+#     print("Number of Features:", n_features)
+#     print("Number of Classes:", n_class)
