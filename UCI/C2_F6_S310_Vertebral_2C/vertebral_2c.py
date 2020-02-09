9e79b19804c4aa0818bf9ed15878981835ce7594
ZealShen <zealshen@outlook.com>
2020-02-09 21:55:13 +0000

    changes

--- UCI/C2_F6_S310_Vertebral_2C/vertebral_2c.py
+++ UCI/C2_F6_S310_Vertebral_2C/vertebral_2c.py
@@ -3,23 +3,23 @@ import io
 import requests
 
 
-def breast_tissue():
+def vertebral_2c():
     file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-                "C2_F6_S310_Vertebral_2C/breast_tissue.csv"
+               "C2_F6_S310_Vertebral_2C/Vertebral_2C.csv"
     s = requests.get(file_url).content
-    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    data = df.values
-    x = data[:, 2:]
-    x = x.astype('float')
-    y_df = df['Class']
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), header=None)
 
-    class_mapping = {'car': 0, 'fad': 1, 'mas': 2, 'gla': 3, 'con': 4, 'adi': 5}
+    x_df = df.iloc[:, 0:6]
+    y_df = df.iloc[:, 6]
+
+    x = x_df.values
+    class_mapping = {'AB': 1, 'NO': 0}
     y = y_df.map(class_mapping).values
     return x, y
 
 
 if __name__ == '__main__':
-    X, Y = breast_tissue()
+    X, Y = vertebral_2c()
     n_samples, n_features = X.shape
     n_class = len(set(Y))
     print("Number of Samples:", n_samples)
