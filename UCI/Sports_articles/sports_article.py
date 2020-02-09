b8813545cff4b66ef5ddf242f42b9b219d8ba93c
ZealShen <zealshen@outlook.com>
2020-02-09 21:03:20 +0000

    sports

---
+++ UCI/Sports_articles/sports_article.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import io
+import requests
+
+
+def sports_article():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+                "C30_F15_S340_Leaf/leaf.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    x_df = df.drop(columns=0)
+    y_df = df.iloc[:, 0]
+
+    x = x_df.values
+    y = y_df.values
+    return x, y
+
+
+if __name__ == '__main__':
+    X, Y = sports_article()
+    n_samples, n_features = X.shape
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
