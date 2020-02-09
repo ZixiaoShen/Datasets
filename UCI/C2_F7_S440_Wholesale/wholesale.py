48daac3a8fca896e9108c8943a72c3dfd8e94daa
ZealShen <zealshen@outlook.com>
2020-02-09 22:21:18 +0000

    wholesale

--- UCI/C2_F7_S440_Wholesale/wholesale.py
+++ UCI/C2_F7_S440_Wholesale/wholesale.py
@@ -3,23 +3,22 @@ import io
 import requests
 
 
-# def appendicitis():
-file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
-           "C2_F7_S440_Wholesale/Wholesale.csv"
-s = requests.get(file_url).content
-df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+def wholesale():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+               "C2_F7_S440_Wholesale/Wholesale.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-    # data = df.values
-    # x = data[:, 0:7]
-    # x = x.astype('float')
-    # y = data[:, 7].astype('int')
-    # return x, y
+    data = df.values
+    x = data[:, 1:]
+    y = data[:, 0] - 1
+    return x, y
 
 
-# if __name__ == '__main__':
-#     X, Y = appendicitis()
-#     n_samples, n_features = X.shape
-#     n_class = len(set(Y))
-#     print("Number of Samples:", n_samples)
-#     print("Number of Features:", n_features)
-#     print("Number of Classes:", n_class)
+if __name__ == '__main__':
+    X, Y = wholesale()
+    n_samples, n_features = X.shape
+    n_class = len(set(Y))
+    print("Number of Samples:", n_samples)
+    print("Number of Features:", n_features)
+    print("Number of Classes:", n_class)
