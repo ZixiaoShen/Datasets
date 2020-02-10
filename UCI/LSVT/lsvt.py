aa8101557d1781db9ac8955cf682c48a9fd8b5f1
ZealShen <zealshen@outlook.com>
2020-02-10 15:45:03 +0000

    changes

---
+++ UCI/LSVT/lsvt.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import io
+import requests
+
+
+# def divorce():
+    file_url = "https://raw.githubusercontent.com/ZixiaoShen/Datasets/master/UCI/" \
+                "C2_F54_S170_Divorce/divorce.csv"
+    s = requests.get(file_url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+    # x_df = df.drop(columns='Class')
+    # y_df = df.iloc[:, -1]
+    #
+    # x = x_df.values
+    # y = y_df.values
+    # return x, y
+
+
+# if __name__ == '__main__':
+#     X, Y = divorce()
+#     n_samples, n_features = X.shape
+#     n_class = len(set(Y))
+#     print("Number of Samples:", n_samples)
+#     print("Number of Features:", n_features)
+#     print("Number of Classes:", n_class)
